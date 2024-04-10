5a5c21e5d781e46f55ee8e3aedd1524172e23889
sm888sm <sm888sm@proton.me>
2024-04-09 23:37:39 -0400

    feat: implement Login backend service API

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const logger = require('../logger');
 const connectToDatabase = require('../models/db');
 
-const JWT_SECRET=`${process.env.JWT_SECRET}`;
+const JWT_SECRET=process.env.JWT_SECRET;
 
 // Register a new user
 router.post('/register', async (req, res) => {
@@ -53,6 +53,50 @@ router.post('/register', async (req, res) => {
     } catch (e) {
         logger.error('Error registering user:', e); // Log the error for debugging
         console.log("error", e)
+        return res.status(500).send('Internal server error');
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to MongoDB 
+        const db = await connectToDatabase(); 
+
+        // Task 2: Access the users collection
+        const collection = db.collection("users");
+
+        // Task 3: Check for user credentials
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        // Task 7: Handle user not found
+        if (!theUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Task 4: Compare the password with the hash
+        const isPasswordValid = await bcryptjs.compare(req.body.password, theUser.password);
+        if (!isPasswordValid) {
+            logger.error('Passwords do not match');
+            return res.status(401).json({ error: 'Invalid email or password' }); // 401 for Unauthorized
+        }
+
+        // Task 5: Fetch user details (adjust if needed)
+        const userName = theUser.firstName;
+        const userEmail = theUser.email;
+
+        // Task 6: Create JWT token 
+        const payload = { 
+            user: {
+                id: theUser._id.toString(), // Convert _id to string
+            },
+        };
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+
+        res.json({ authtoken, userName, userEmail });
+
+    } catch (e) {
+        logger.error('Error logging in user:', e);
         return res.status(500).send('Internal server error');
     }
 });
